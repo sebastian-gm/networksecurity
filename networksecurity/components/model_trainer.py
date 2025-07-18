f843dc5ad52f3c6be40529cf519a216f30e6871f
Sebastian <sebastian.sgm@outlook.com>
2025-07-18 14:08:32 -0700

    started model trainer

---
+++ networksecurity/components/model_trainer.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+from networksecurity.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
+
+from networksecurity.utils.main_utils.utils import save_object, load_object
+from networksecurity.utils.main_utils.utils import load_numpy_array_data
+from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
+
+class ModelTrainer:
+    def __init__(self, model_trainer_config: ModelTrainerConfig, data_transformation_artifact: DataTransformationArtifact):
+        try:
+            self.model_trainer_config = model_trainer_config
+            self.data_transformation_artifact = data_transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def train_model(self, X_train, y_train):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def initiate_model_trainer(self) -> ModelTrainerArtifact:
+        try:
+            train_file_path = self.data_transformation_artifact.transformed_train_file_path
+            test_file_path = self.data_transformation_artifact.transformed_test_file_path
+
+            #loading training array and testing array
+            train_arr = load_numpy_array_data(train_file_path)
+            test_arr = load_numpy_array_data(test_file_path)
+
+            x_train, y_train, x_test, y_test = (
+                train_arr[:, :-1],
+                train_arr[:, -1],
+                test_arr[:, :-1],
+                test_arr[:, -1]
+            )
+
+            model = self.train_model(x_train, y_train)
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
