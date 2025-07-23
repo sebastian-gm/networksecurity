e20d0cbd7ce7a6adb5204d7d6bdd87e67d9d99ca
Sebastian <sebastian.sgm@outlook.com>
2025-07-23 15:59:39 -0700

    pipeline is completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -106,7 +106,7 @@ class ModelTrainer:
         network_model = NetworkModel(preprocessor=preprocessor, model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=network_model)
 
-        save_object("final_models/model.pkl", best_model)
+        save_object("final_model/model.pkl", best_model)
 
         # Model Trainer Artifact
         model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
